2ebf2199d3a0e8c7a4d6dbb0bb9d16123fca83d8
dadagofafa <fu6fu62022@gmail.com>
2021-11-04 20:44:08 +0800

    fix error for type and module.exports

--- models/Restaurant.js
+++ models/Restaurant.js
@@ -2,15 +2,15 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const restaurantSchema = new Schema({
-  name: {type: String, required: true},
-  name_en: {type: String, required: true},
+  name: { type: String, required: true },
+  name_en: { type: String, required: true },
   category: { type: String, required: true },
   image: { type: String, required: true },
   location: { type: String, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: String, required: true },
   google_map: { type: String, required: true },
   rating: { type: Number, required: true },
   description: { type: String, required: true },
 })
 
-module.export = mongoose.model('Restaurant', restaurantSchema)
+module.exports = mongoose.model('Restaurant', restaurantSchema)
